35185729e095715aafc189e668ded92719218da0
Mateusz Mizerski <106737627+mateuszmizer@users.noreply.github.com>
2022-07-22 14:53:51 +0200

    update policy executor

--- policy_executor/scripts/labeler.py
+++ policy_executor/scripts/labeler.py
@@ -41,5 +41,6 @@ class Labeler:
 
 if __name__=='__main__':
     deployment_id = inputs.get('deployment_id')
-    sla_labels = [inputs.get('SLA_VALUE')]
+    sla_labels = [{'SLA_POLICY': inputs.get('SLA_VALUE')}]
+    ctx_logger.info(f'SLA: {sla_labels}')
     Labeler().update_deployment_labels(deployment_id=deployment_id, labels=sla_labels)
