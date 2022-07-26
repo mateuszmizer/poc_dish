aa3b4b9bbace7cf1bf52b6fa9d68ce00160bdb90
Mateusz Mizerski <106737627+mateuszmizer@users.noreply.github.com>
2022-07-26 08:15:47 +0200

    aws cli installlator add

---
+++ aws_cli_installator/scripts/aws_cli_installer.sh
@@ -0,0 +1,10 @@
+# support only centos installation
+ctx logger info "install unzip"
+yum install unzip -y
+ctx logger info "upload aws cli package"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/instal
+aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
+aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
+aws configure set default.region ${AWS_DEFAULT_REGION}
