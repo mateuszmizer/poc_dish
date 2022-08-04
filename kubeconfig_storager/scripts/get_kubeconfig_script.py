eb1c86c1c38e81c7be0c4119fc2eb13ac0f4205b
Mateusz Mizerski <106737627+mateuszmizer@users.noreply.github.com>
2022-08-04 13:53:59 +0200

    update

--- kubeconfig_storager/scripts/get_kubeconfig_script.py
+++ kubeconfig_storager/scripts/get_kubeconfig_script.py
@@ -15,7 +15,7 @@ runner = cloudify_utils.LocalCommandRunner(ctx_logger)
 def get_aws_eks_kubeconfig(region: str, eks_name: str, file: str):
     cmd = f'aws eks update-kubeconfig --region {region} --name {eks_name} --dry-run'
     response = runner.run(cmd)
-    runner.run(f'echo {response.stdout}|tee {file}')
+    runner.run(f'echo {response.std_out}|tee {file}')
     return response.std_out.replace('client.authentication.k8s.io/v1beta1', 'client.authentication.k8s.io/v1alpha1')  # WA for helm issue
 
 
