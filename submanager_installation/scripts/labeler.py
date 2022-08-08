d88d0ccc858d8dc6b33ccb72d6382512f5824ac6
Mateusz Mizerski <106737627+mateuszmizer@users.noreply.github.com>
2022-08-08 12:57:52 +0200

    update

--- submanager_installation/scripts/labeler.py
+++ submanager_installation/scripts/labeler.py
@@ -41,6 +41,6 @@ class Labeler:
 
 if __name__=='__main__':
     deployment_id = inputs.get('deployment_id')
-    sla_labels = [{'SLA_POLICY': inputs.get('SLA_VALUE')}]
+    sla_labels = [{'SLA_POLICY': str(inputs.get('SLA_VALUE').split(','))}]
     ctx_logger.info(f'SLA: {sla_labels}')
-    Labeler().update_deployment_labels(deployment_id=deployment_id, labels=sla_labels.split(','))
+    Labeler().update_deployment_labels(deployment_id=deployment_id, labels=sla_labels)
